3d0f145b731b611ee64c56fe7ca5a1e4d28b03fe
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-03 17:29:45 -0400

    Initial Push

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,3 +1,5 @@
+const Schemes = require("./scheme-model");
+
 /*
   If `scheme_id` does not exist in the database:
 
@@ -6,9 +8,8 @@
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
 
-}
+const checkSchemeId = async (req, res, next) => {};
 
 /*
   If `scheme_name` is missing, empty string or not a string:
@@ -18,9 +19,7 @@ const checkSchemeId = (req, res, next) => {
     "message": "invalid scheme_name"
   }
 */
-const validateScheme = (req, res, next) => {
-
-}
+const validateScheme = (req, res, next) => {};
 
 /*
   If `instructions` is missing, empty string or not a string, or
@@ -31,12 +30,10 @@ const validateScheme = (req, res, next) => {
     "message": "invalid step"
   }
 */
-const validateStep = (req, res, next) => {
-
-}
+const validateStep = (req, res, next) => {};
 
 module.exports = {
   checkSchemeId,
   validateScheme,
   validateStep,
-}
+};
