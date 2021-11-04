91d415195d41631415889143892eca49e768fc62
George Cavazos <cavazosgeorge@yahoo.com>
2021-11-03 20:37:36 -0400

    Added global error route handler

--- api/server.js
+++ api/server.js
@@ -1,10 +1,18 @@
-const express = require('express');
+const express = require("express");
 
-const SchemeRouter = require('./schemes/scheme-router.js');
+const SchemeRouter = require("./schemes/scheme-router.js");
 
 const server = express();
 
 server.use(express.json());
-server.use('/api/schemes', SchemeRouter);
+server.use("/api/schemes", SchemeRouter);
 
+// eslint-disable-next-line
+server.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message,
+    stack: err.stack,
+  });
+});
 module.exports = server;
